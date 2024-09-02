12ee027b9660bdd25b365bfe6279e69a005e3eab
uxodb <uxodb@x0rmail.com>
2024-09-03 01:32:59 +0200

    edit role

--- migrations/20240612123726_enable_rls_update_grants.up.sql
+++ migrations/20240612123726_enable_rls_update_grants.up.sql
@@ -16,21 +16,21 @@ do $$ begin
     alter table {{ index .Options "Namespace" }}.flow_state enable row level security;
     alter table {{ index .Options "Namespace" }}.identities enable row level security;
     alter table {{ index .Options "Namespace" }}.one_time_tokens enable row level security;
-    -- allow postgres role to select from auth tables and allow it to grant select to other roles
-    grant select on {{ index .Options "Namespace" }}.schema_migrations to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.instances to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.users to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.audit_log_entries to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.saml_relay_states to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.refresh_tokens to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.mfa_factors to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.sessions to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.sso_providers to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.sso_domains to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.mfa_challenges to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.mfa_amr_claims to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.saml_providers to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.flow_state to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.identities to postgres with grant option;
-    grant select on {{ index .Options "Namespace" }}.one_time_tokens to postgres with grant option;
+    -- allow supabase_auth_admin role to select from auth tables and allow it to grant select to other roles
+    grant select on {{ index .Options "Namespace" }}.schema_migrations to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.instances to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.users to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.audit_log_entries to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.saml_relay_states to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.refresh_tokens to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.mfa_factors to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.sessions to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.sso_providers to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.sso_domains to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.mfa_challenges to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.mfa_amr_claims to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.saml_providers to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.flow_state to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.identities to supabase_auth_admin with grant option;
+    grant select on {{ index .Options "Namespace" }}.one_time_tokens to supabase_auth_admin with grant option;
 end $$;
